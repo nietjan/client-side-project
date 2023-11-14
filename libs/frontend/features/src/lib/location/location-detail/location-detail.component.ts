229d2c835fa3bbd963a17509fa62e7386c982473
Jantje <jtb.vanberkel@student.avans.nl>
2023-11-14 13:40:16 +0100

    added detail component initialze

---
+++ libs/frontend/features/src/lib/location/location-detail/location-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'client-side-project-location-detail',
+  templateUrl: './location-detail.component.html',
+  styleUrls: ['./location-detail.component.css'],
+})
+export class LocationDetailComponent {}
