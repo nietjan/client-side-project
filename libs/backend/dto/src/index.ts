df7063168fcd51d4c268d3ef2d2c441a4c4b14e8
Jantje <jtb.vanberkel@student.avans.nl>
2023-11-20 10:59:51 +0100

    added abonomment service

--- libs/backend/dto/src/index.ts
+++ libs/backend/dto/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/dto.module';
 export * from './lib/api-reponse.interceptor';
 export * from './lib/location.dto';
 export * from './lib/user.dto';
+export * from './lib/abonnement.dto';
--- libs/frontend/features/src/lib/abonnement/abonnement.services.ts
+++ libs/frontend/features/src/lib/abonnement/abonnement.services.ts
@@ -1,20 +1,80 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IAbonnement } from '@client-side/shared/api';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 
 @Injectable()
 export class AbonnementService {
   endpoint = 'http://localhost:3000/api/location';
 
+  private abonnements$ = new BehaviorSubject<IAbonnement[]>([]);
+
   constructor(private readonly http: HttpClient) {}
 
-  // public createAbonnement(location: ILocation[] | null): string | null {
-  //   if (location == null) return null;
+  public allLocations(
+    locationId: string | null,
+    options?: any
+  ): Observable<IAbonnement[] | null> {
+    // console.log(`list ${this.endpoint}`);
 
-  //   location.id = (
-  //     parseInt(this.locations$.value[this.locations$.value.length - 1].id) + 1
-  //   ).toString();
+    // return this.http
+    //   .get<ApiResponse<ILocation[]>>(this.endpoint, {
+    //     ...options,
+    //     ...httpOptions,
+    //   })
+    //   .pipe(
+    //     map((response: any) => response.results as ILocation[]),
+    //     tap(console.log),
+    //     catchError(this.handleError)
+    //   );
+    return this.abonnements$.pipe(
+      map((abonnementList) =>
+        abonnementList.filter(
+          (abonnement) => abonnement.locationId == locationId
+        )
+      )
+    );
+  }
 
-  //   this.locations$.next([...this.locations$.value, location]);
-  //   return location.id;
-  // }
+  public createAbonnement(abonomentArr: IAbonnement[] | null): boolean {
+    if (abonomentArr == null) return false;
+
+    this.abonnements$.next([...this.abonnements$.value, ...abonomentArr]);
+
+    return true;
+  }
+
+  public removeAbonnement(
+    locationId: string | null,
+    period: number | null
+  ): boolean {
+    if (locationId == null || period == null) return false;
+
+    const arr: any[] = this.abonnements$.getValue();
+
+    arr.forEach((item, index) => {
+      if (item.id == locationId && item.period == period) {
+        arr.splice(index, 1);
+      }
+    });
+
+    this.abonnements$.next(arr);
+    return true;
+  }
+
+  public updateAbonnement(abonnement: IAbonnement | null): boolean {
+    if (abonnement?.locationId == null || abonnement?.period == null)
+      return false;
+
+    //delete
+    let succes: boolean = this.removeAbonnement(
+      abonnement.locationId,
+      abonnement.period
+    );
+    if (!succes) return false;
+
+    //add it again
+    this.abonnements$.next([...this.abonnements$.value, abonnement]);
+    return true;
+  }
 }
